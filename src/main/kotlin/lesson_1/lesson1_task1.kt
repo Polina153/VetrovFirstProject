c040fc6de8d904dd72357abf74e3418cf245ad2b
Linka <Polina153@mail.ru>
2025-03-05 15:26:51 +0400

    VFP-1-1 task is done

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,6 @@
 package org.example.lesson_1
 
 fun main(){
-    println("Good luck")
+    var onlineOrders = 75
+    val thankYouPhrase = "Thank you for your order! Partnership with you is important for us, will be happy to serve you again!"
 }
