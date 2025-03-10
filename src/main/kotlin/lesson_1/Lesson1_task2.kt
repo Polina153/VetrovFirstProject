edd94f0b890ecfadf1f316bb5d4330bdff2c0edd
Linka <Polina153@mail.ru>
2025-03-10 19:51:45 +0400

    VFP-1-2 variables are printed out

---
+++ src/main/kotlin/lesson_1/Lesson1_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    //Variables
+    val onlineOrders = 75
+    val thankYouPhrase =
+        "Thank you for your order! Partnership with you is important for us, will be happy to serve you again!"
+    var onlineEmployees = 2000
+
+
+    println(onlineOrders)
+    println(thankYouPhrase)
+    //println(onlineEmployees)
+    onlineEmployees--
+    println(onlineEmployees)
+}
